a5f9b3cc3a4689ca5e0731e6d2421c6edc08c6ee
xaviertoala <xaviertoala2006@gmail.com>
2025-07-08 21:03:21 -0500

    Sistema

--- src/main/java/ec/gob/orellana/www/sistemaacademico/SistemaAcademico.java
+++ src/main/java/ec/gob/orellana/www/sistemaacademico/SistemaAcademico.java
@@ -33,8 +33,8 @@ public class SistemaAcademico {
         VEstudiantes x = new VEstudiantes();
         x.setVisible(true);
         
-        //VProfesores p = new VProfesores ();
-        //p.setVisible(true);
+        VProfesores p = new VProfesores ();
+        p.setVisible(true);
         
         //VPersonasInvitadas i = new VPersonasInvitadas ();
         //i.setVisible(true);
